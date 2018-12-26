838d5d3304d1ec5b684a987dd4806cc574a96a64
James <james.rudrum@hyperisland.se>
2018-12-26 17:01:39 +0000

    fizzbuzz works for all numbers

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,17 @@
 require 'fizzbuzz'
 
+
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when divisible by 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed 5' do
+  it 'returns "buzz" when divisible by 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizbuzz" when divisible by 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns number given if indivisible by 3 or 5' do
+    expect(fizzbuzz(8)).to eq 8
   end
 end
